602e54b53965b09c8080f4cc8235e1bf9d58608b
Titouan <titouan.alvares@etu-univ-amu.fr>
2024-04-18 12:09:45 +0200

    chore: affichage et conteneurs

--- src/main/java/fr/amu/iut/exercice2/TicTacToe.java
+++ src/main/java/fr/amu/iut/exercice2/TicTacToe.java
@@ -1,12 +1,10 @@
 package fr.amu.iut.exercice2;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import java.util.Random;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -14,18 +12,13 @@ public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // conteneur principal
-        VBox vBox = new VBox();
-
         // objet de Random pour choisir l'image à afficher
         Random random = new Random();
         // objet de GridPane servant de grille de jeu
         GridPane grille = new GridPane();
         grille.setGridLinesVisible(true);
-        grille.setAlignment(Pos.CENTER);
         for (int i = 0; i<9; ++i) {
-            int nombre = random.nextInt(3);
-            switch (nombre) {
+            switch (random.nextInt(3)) {
                 // selon la valeur de i on ajoute l'image dans une cellule différente
                 case 0:
                     grille.add(new Label("", new ImageView("exercice2/Croix.png")), i/3, i%3);
@@ -39,16 +32,11 @@ public class TicTacToe extends Application {
             }
         }
 
-        // ajout de la grille au conteneur principal
-        vBox.getChildren().add(grille);
-
-        // création de la scène avec le conteneur principal
-        Scene scene = new Scene(vBox);
+        // création de la scène avec la grille
+        Scene scene = new Scene(grille);
         // ajout de la scène à Stage
         primaryStage.setScene(scene);
         // paramètrage du Stage
-        primaryStage.setHeight(178);
-        primaryStage.setWidth(133);
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setResizable(false);
         // affichage de la page
