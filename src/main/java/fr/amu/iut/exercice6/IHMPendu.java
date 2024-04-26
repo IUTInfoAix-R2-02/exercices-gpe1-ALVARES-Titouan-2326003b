5950cc1fe0985ce59cb6df28f68bc4a7866722d6
Titouan <titouan.alvares@etu.univ-amu.fr>
2024-04-26 11:53:14 +0200

    feat: utilisation du clavier physique possible + fix: résidus des parties précédentes dans les nouveaux mots retirés

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -3,10 +3,13 @@ package fr.amu.iut.exercice6;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -35,18 +38,22 @@ public class IHMPendu extends Application {
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
+
         // initialisation de boutonsUtilises
         boutonsUtilises = new ArrayList<>();
         // initilisation de dico
         dico = new Dico();
+
         // listes qui stockent les lettres de chaque bouton par ligne du clavier
         ArrayList<String> listeClavier1 = new ArrayList<>(Arrays.asList("a", "e", "i", "o", "u", "y"));
         ArrayList<String> listeClavier2 = new ArrayList<>(Arrays.asList("b", "c", "d", "f", "g", "h", "j", "k", "l", "m"));
         ArrayList<String> listeClavier3 = new ArrayList<>(Arrays.asList("n", "p", "q", "r", "s", "t", "v", "w", "x", "z"));
+
         // initialisation du nombre de vies à 7
         nbVies = 7;
         // initialisation du mot à trouver
         mot = new StringBuilder(dico.getMot());
+
 
         // initialisation du conteneur principal
         VBox vBox = new VBox();
@@ -54,62 +61,74 @@ public class IHMPendu extends Application {
         // initialisation du conteneur affichant le pendu, le mot et le nombre de vies
         VBox pendu = new VBox();
         pendu.setAlignment(Pos.CENTER);
+
         // initialisation de l'image du pendu, correspondant au départ au nombre de vies maximum
         imgPendu = new Label();
         imgPendu.setGraphic(new ImageView("exercice6/pendu7.png"));
+
         // initialisation du label affichant le nombre de vies restantes, au départ à 7
         vie = new Label("Nombre de vies : 7");
         vie.setStyle("-fx-font-weight: bold;" +
                 "-fx-font-size: 20;");
+
         // initialisation de l'affichage du mot, au départ composé uniquement de *
         motRevele = new StringBuilder(mot.length());
         for (int i = 0; i < mot.length(); ++i) motRevele.insert(0, "*");
+
         // initialisation du label affichant le mot
         motCache = new Label(motRevele.toString());
         motCache.setStyle("-fx-font-weight: bold;" +
                 "-fx-font-size: 30;");
+
         // initialisation de etatDeJeu
         etatDeJeu = new Label();
         etatDeJeu.setStyle("-fx-font-weight: bold;" +
                 "-fx-font-size: 20;");
+
         // ajout des noeuds précédents au conteneur pendu
         pendu.getChildren().addAll(imgPendu, vie, motCache, etatDeJeu);
+
 
         // initialisation du conteneur affichant les boutons
         VBox boutons = new VBox();
         boutons.setAlignment(Pos.CENTER);
+
         // initialisation du conteneur affichant la première ligne du clavier
         HBox ligne1 = new HBox();
         ligne1.setAlignment(Pos.CENTER);
-        // chaque lettre est associée à un bouton ajouté à la première ligne du clavier
+        // chaque lettre est associée à un bouton de la première ligne du clavier
         for (String lettre : listeClavier1) {
             Button touche = new Button(lettre);
             // méthode attribuant un style et un eventHandler à chaque bouton
             initButton(touche);
             ligne1.getChildren().add(touche);
         }
+
         // initialisation du conteneur affichant la deuxième ligne du clavier
         HBox ligne2 = new HBox();
         ligne2.setAlignment(Pos.CENTER);
-        // chaque lettre est associée à un bouton ajouté à la deuxième ligne du clavier
+        // chaque lettre est associée à un bouton de la deuxième ligne du clavier
         for (String lettre : listeClavier2) {
             Button touche = new Button(lettre);
             // méthode attribuant un style et un eventHandler à chaque bouton
             initButton(touche);
             ligne2.getChildren().add(touche);
         }
+
         // initialisation du conteneur affichant la troisième ligne du clavier
         HBox ligne3 = new HBox();
         ligne3.setAlignment(Pos.CENTER);
-        // chaque lettre est associée à un bouton ajouté à la troisième ligne du clavier
+        // chaque lettre est associée à un bouton de la troisième ligne du clavier
         for (String lettre : listeClavier3) {
             Button touche = new Button(lettre);
             // méthode attribuant un style et un eventHandler à chaque bouton
             initButton(touche);
             ligne3.getChildren().add(touche);
         }
+
         // initialisation d'un bouton rejouer
         Button rejouer = new Button("Rejouer");
+
         // attribution d'une forme de rectangle avec les coins arrondis
         Rectangle formeBouton = new Rectangle(3, 1);
         formeBouton.setArcWidth(1.5);
@@ -122,25 +141,55 @@ public class IHMPendu extends Application {
                 "-fx-font-weight: bold;" +
                 "-fx-font-size: 15.0");
         rejouer.setAlignment(Pos.BOTTOM_CENTER);
+
         // attribution de l'évènement rejouerEvent au bouton rejouer quand il est cliqué
         rejouer.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> rejouerEvent());
+
         // ajout des noeuds précédents au conteneur boutons
         boutons.getChildren().addAll(ligne1, ligne2, ligne3, rejouer);
 
+
         // ajout de pendu et boutons au conteneur principal
         vBox.getChildren().addAll(pendu, boutons);
-        // les conteneurs pendu et boutons bougeront selon la hauteur de la fenêtre
+
+
+        // la position des conteneurs pendu et boutons dépendra de la hauteur de la fenêtre
         VBox.setVgrow(pendu, Priority.ALWAYS);
         VBox.setVgrow(boutons, Priority.ALWAYS);
+
         // ajout de marges au différents noeuds
         VBox.setMargin(vie, new Insets(10.0, 0.0, 10.0, 0.0));
         VBox.setMargin(motCache, new Insets(5.0, 0.0, 10.0, 0.0));
         VBox.setMargin(rejouer, new Insets(15.0, 0.0, 10.0, 0.0));
         VBox.setMargin(boutons, new Insets(15.0, 0.0, 10.0, 0.0));
+
         // le fond de la page est cyan clair
         vBox.setStyle("-fx-background-color: lightcyan");
+
         // initialisation de la scène avec le conteneur principal
         Scene scene = new Scene(vBox);
+        // ajout d'un gestionnaire d'évènement à scene quand une touche du clavier physique est appuyée
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            String carac = event.getText();
+            if (listeClavier1.contains(carac)) {            // si la touche fait partie de la première ligne du clavier affiché, alors lettreEvent est appelé avec en paramètre la touche de la première ligne du clavier virtuel correspondant à celle de event
+                for (Node bouton1 : ligne1.getChildren()) if (bouton1 instanceof Button && ((Button) bouton1).getText().equals(carac)) {
+                    lettreEvent((Button) bouton1);
+                    break;      // si la touche a été trouvée, alors on arrête la recherche
+                }
+            } else if (listeClavier2.contains(carac)) {     // si la touche fait partie de la deuxième ligne du clavier affiché, alors lettreEvent est appelé avec en paramètre la touche de la deuxième ligne du clavier virtuel correspondant à celle de event
+                for (Node bouton2 : ligne2.getChildren()) if (bouton2 instanceof Button && ((Button) bouton2).getText().equals(carac)) {
+                    lettreEvent((Button) bouton2);
+                    break;      // si la touche a été trouvée, alors on arrête la recherche
+                }
+            } else if (listeClavier3.contains(carac)) {     // si la touche fait partie de la troisième ligne du clavier affiché, alors lettreEvent est appelé avec en paramètre la touche de la troisième ligne du clavier virtuel correspondant à celle de event
+                for (Node bouton3 : ligne3.getChildren()) if (bouton3 instanceof Button && ((Button) bouton3).getText().equals(carac)) {
+                    lettreEvent((Button) bouton3);
+                    break;      // si la touche a été trouvée, alors on arrête la recherche
+                }
+            } else if (event.getCode() == KeyCode.BACK_SPACE) rejouerEvent();       // si la touche pressée correspond à backspace, alors rejouerEvent est appelé et une nouvelle partie commence
+            else if (event.getCode() == KeyCode.DELETE) primaryStage.close();       // si la touche pressée correspond à la touche suppr, alors la fenêtre est fermée et le jeu se termine
+        });
+
         // ajout de la scène
         primaryStage.setScene(scene);
         // affichage du stage
@@ -155,8 +204,10 @@ public class IHMPendu extends Application {
         formeBouton.setArcWidth(1.0);
         bouton.setShape(formeBouton);
         bouton.setMinSize(48.0, 48.0);
+
         // méthode attribuant un style au bouton
         initStyleButton(bouton);
+
         // attribution de l'évènement lettreEvent au bouton quand il est cliqué
         bouton.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> lettreEvent(bouton));
     }
@@ -176,10 +227,12 @@ public class IHMPendu extends Application {
         if (nbVies < 1
                 || boutonsUtilises.contains(bouton)
                 || !motRevele.toString().contains("*")) return;
+
         // si le bouton cliqué est celui d'une lettre présente dans le mot, alors toutes ses occurrences sont révélées à l'affichage
         if (mot.toString().contains(bouton.getText())) {
             // initialisation d'une variable contenant la première occurrence du caractère dans le mot
             int index = mot.indexOf(bouton.getText());
+
             // tant qu'il y a des occurrences, la boucle continue de tourner
             while (index > -1) {
                 // on remplace l'occurrence du caractère à l'indice index du mot affiché
@@ -189,31 +242,39 @@ public class IHMPendu extends Application {
                 // on met à jour index en lui attribuant l'indice de la prochaine occurrence du caractère
                 index = mot.indexOf(bouton.getText(), index+1);
             }
+
             // on met à jour l'affichage
             motCache.setText(motRevele.toString());
+
             // on affiche un message indiquant au joueur qu'il a gagné si l'affichage du mot est complet
             if (!motRevele.toString().contains("*")) etatDeJeu.setText("Gagné!");
         }
+
         // si le bouton cliqué n'est pas celui d'une lettre présente dans le mot, alors le joueur perd une vie
         else {
             // décrémentation du nombre de vies
             --nbVies;
+
             // si le nombre de vies atteint 0, alors un affichage spécial apparaît
             if (nbVies == 0) {
                 vie.setText("Perdu");
                 etatDeJeu.setText("Le mot était " + mot);
             }
+
             // sinon on met à jour l'affichage du nombre de vies
             else vie.setText("Nombre de vies : " + nbVies);
+
             // mise à jour de l'image du pendu
             imgPendu.setGraphic(new ImageView("exercice6/pendu" + nbVies + ".png"));
         }
+
         // mise à jour de l'affichage du bouton maintenant qu'il a été utilisé
         bouton.setStyle("-fx-background-color: lightcyan ;" +
                 "-fx-border-color: rgba(255,165,0,0.5) ;" +
                 "-fx-text-fill: rgba(0,139,139,0.5) ;" +
                 "-fx-font-weight: bold;" +
                 "-fx-font-size: 20.0");
+
         // ajout du bouton à la liste des boutons utilisés durant la partie
         boutonsUtilises.add(bouton);
     }
@@ -224,10 +285,10 @@ public class IHMPendu extends Application {
         vie.setText("Nombre de vies : 7");      // l'affichage du nombre de vies est réinitialisé
         imgPendu.setGraphic(new ImageView("exercice6/pendu7.png"));     // l'affichage du pendu est réinitialisé
         etatDeJeu.setText("Le mot précédent était " + mot);         // affichage spécial : affichage du mot de la partie venant de se terminer
+        motRevele.delete(0, mot.length());      // le stringbuilder contenant le mot affiché est vidé
         mot.delete(0, mot.length());    // le stringbuilder contenant le mot à trouver est vidé
         mot.append(dico.getMot());      // puis re-rempli avec un autre mot pour la partie suivante
-        motRevele.delete(0, mot.length());      // le stringbuilder contenant le mot affiché est vidé
-        for (int i = 0; i < mot.length(); ++i) motRevele.insert(0, "*");    // puis re-rempli avec des *
+        for (int i = 0; i < mot.length(); ++i) motRevele.insert(0, "*");    // le stringbuilder contenant le mot affiché est re-rempli avec des *
         motCache.setText(motRevele.toString());     // l'affichage du mot est mis à jour
         for (Button boutonUtilise : boutonsUtilises) initStyleButton(boutonUtilise);    // le style des boutons utilisés lors de la partie est réinitialisé
         boutonsUtilises.clear();        // la liste des boutons utilisés est vidée pour la partie suivante
