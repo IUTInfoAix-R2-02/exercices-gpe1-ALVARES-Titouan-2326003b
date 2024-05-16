f5c3461fbb0304bd395c969db0d9719cba3b7e36
Titouan <titouan.alvares@etu-univ-amu.fr>
2024-05-16 11:46:25 +0200

    feat: boutons fonctionnels

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,31 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
+    @FXML
+    TextField user;
+    @FXML
+    PasswordField pwd;
+    @FXML
+    Button ok;
+    @FXML
+    Button cancel;
 
+    @FXML
     private void okClicked() {
+        System.out.println(user.getText());
+        for (int i = 0; i<pwd.getText().length(); ++i) System.out.print("*");
+        System.out.println();
     }
 
+    @FXML
     private void cancelClicked() {
+        user.setText("");
+        pwd.setText("");
     }
 }
